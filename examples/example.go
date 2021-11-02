959d8640fa60e2bcaafbe79168821a1fdc71c41c
huzhuo <huzhuo@kingsoft.com>
2021-11-02 21:27:23 +0800

    修改example用例

--- examples/example.go
+++ examples/example.go
@@ -14,50 +14,50 @@ var _ = plugin.Description{}
 
 var _ = plugin.Description{}
 
-// Annotation@Description={"body":"a"}
-type A struct {
-	FieldA string
-}
-
-// Annotation@Description={"body":"b"}
-type B struct {
-	FieldB string
-}
-
-// Annotation@Description={"body":"b"}
-type C interface {
-}
-
-// Annotation@Component
-type ComponentA struct {
-	B1 *ComponentB `autowired:"true"` // Will populate with new(ComponentB)
-	B2 *ComponentB `autowired:"true"` // Will populate with new(ComponentB)
-	B3 *ComponentB
-}
-
-// Annotation@Component={"type": "Singleton"}
-type ComponentB struct {
-	C *ComponentC `autowired:"true"` // Will populate with NewComponentC()
-}
-
-// Annotation@Component
-type ComponentC struct {
-	D        *ComponentD `autowired:"true"` // Will populate with NewComponentD()
-	IntValue int
-}
-
-func NewComponentC() *ComponentC {
-	return &ComponentC{IntValue: 1}
-}
-
-// Annotation@Component
-type ComponentD struct {
-	IntValue int
-}
-
-func NewComponentD() (*ComponentD, error) {
-	return &ComponentD{IntValue: 2}, nil
-}
+//// Annotation@Description={"body":"a"}
+//type A struct {
+//	FieldA string
+//}
+//
+//// Annotation@Description={"body":"b"}
+//type B struct {
+//	FieldB string
+//}
+//
+//// Annotation@Description={"body":"b"}
+//type C interface {
+//}
+//
+//// Annotation@Component
+//type ComponentA struct {
+//	B1 *ComponentB `autowired:"true"` // Will populate with new(ComponentB)
+//	B2 *ComponentB `autowired:"true"` // Will populate with new(ComponentB)
+//	B3 *ComponentB
+//}
+//
+//// Annotation@Component={"type": "Singleton"}
+//type ComponentB struct {
+//	C *ComponentC `autowired:"true"` // Will populate with NewComponentC()
+//}
+//
+//// Annotation@Component
+//type ComponentC struct {
+//	D        *ComponentD `autowired:"true"` // Will populate with NewComponentD()
+//	IntValue int
+//}
+//
+//func NewComponentC() *ComponentC {
+//	return &ComponentC{IntValue: 1}
+//}
+//
+//// Annotation@Component
+//type ComponentD struct {
+//	IntValue int
+//}
+//
+//func NewComponentD() (*ComponentD, error) {
+//	return &ComponentD{IntValue: 2}, nil
+//}
 
 // Annotation@Component
 type ApplicationHandlerService struct {
@@ -66,6 +66,7 @@ type ApplicationHandlerService struct {
 }
 
 // Annotation@Service
+// Annotation@Component
 type ApplicationHandler struct {
 }
 
--- go.mod
+++ go.mod
@@ -2,13 +2,10 @@ module github.com/fourhu/go-annotation
 
 go 1.14
 
-replace github.com/u2takey/go-annotation v0.0.0-20200530160507-bccb59c601c4 => github.com/fourhu/go-annotation v0.0.0-20211102122029-cf4639ca439e
-
 require (
 	github.com/mj37yhyy/gowb v0.0.0-20200901033728-a9b98f7f1b9d
 	github.com/spf13/pflag v1.0.5
-	github.com/u2takey/go-annotation v0.0.0-20200530160507-bccb59c601c4
-	k8s.io/gengo v0.0.0-20200428234225-8167cfdcfc14
+	k8s.io/gengo v0.0.0-20211102085258-f2aa3d7bd7d5
 	k8s.io/klog v1.0.0
-	k8s.io/klog/v2 v2.0.0
+	k8s.io/klog/v2 v2.2.0
 )
